543a91e799e41771e7de85b0546f408d21bfb032
tylerc122 <tylercollo1@gmail.com>
2025-04-06 15:23:58 -0400

    Getting ready to deploy

--- backend/app/main.py
+++ backend/app/main.py
@@ -37,7 +37,7 @@ app.add_middleware(RateLimitMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174", "https://grade-calculator.vercel.app", "https://gradeflow.org"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
